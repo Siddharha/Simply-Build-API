72e9d64f73e914731603a0806e74ef7b5f81aab4
siddhartha <siddhartha.maji@bluehorse.in>
2017-04-25 15:44:58 +0530

    Initialize Message

--- src/simply/build/api/SimplyBuildAPI.java
+++ src/simply/build/api/SimplyBuildAPI.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -25,6 +26,11 @@ public class SimplyBuildAPI extends Application {
         Scene scene = new Scene(root);
          stage.initStyle(StageStyle.DECORATED);
         stage.setTitle("Simply Build API");
+        stage.getIcons().addAll(
+        new Image("/resources/smp_api_16x16.png"),
+        new Image("/resources/smp_api_32x32.png"),
+        new Image("/resources/smp_api_64x64.png")
+);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
